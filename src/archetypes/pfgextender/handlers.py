40b45d2328a4517d717d9eda2128c6de8495b51e
gotcha <gotcha@12379f12-61e2-0310-a953-c5ab0a7db1c5>
2010-11-09 12:42:03 +0000

    add handler for IObjectMovedEvent
    so that Form remain registered even when renamed
    
    git-svn-id: https://svn.plone.org/svn/archetypes/archetypes.pfgextender/trunk@13222 12379f12-61e2-0310-a953-c5ab0a7db1c5

--- src/archetypes/pfgextender/handlers.py
+++ src/archetypes/pfgextender/handlers.py
@@ -1,18 +1,28 @@
 from zope.component import getSiteManager
 from zope.interface import alsoProvides
 
-from Products.PloneFormGen.interfaces import IPloneFormGenForm
-
 from archetypes.pfgextender.tool import PFGExtenderTool
 from archetypes.pfgextender.interfaces import IPFGExtenderForm
 
 
 def addedForm(obj, event):
+    """registered for IPloneFormGenForm
+    """
     parent = event.newParent
-    if not isinstance(parent, PFGExtenderTool):
-        return
-    if not IPloneFormGenForm.providedBy(obj):
-        return
-    alsoProvides(obj, IPFGExtenderForm)
+    if isinstance(parent, PFGExtenderTool):
+        alsoProvides(obj, IPFGExtenderForm)
+        sm = getSiteManager(obj)
+        sm.registerUtility(obj, provided=IPFGExtenderForm, name=obj.getId())
+
+
+def movedForm(obj, event):
+    """registered for IPFGExtenderForm
+    """
+    parent = event.oldParent
     sm = getSiteManager(obj)
-    sm.registerUtility(obj, provided=IPFGExtenderForm, name=obj.getId())
+    if isinstance(parent, PFGExtenderTool):
+        sm.unregisterUtility(obj, provided=IPFGExtenderForm,
+            name=event.oldName)
+    parent = event.newParent
+    if isinstance(parent, PFGExtenderTool):
+        sm.registerUtility(obj, provided=IPFGExtenderForm, name=event.newName)
